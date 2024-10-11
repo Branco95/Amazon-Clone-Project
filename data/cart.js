50bef08d836937c0c1022e450116aee4b782f587
Branco95 <branquinho95@outlook.com>
2024-10-11 17:41:00 +0100

    Saave data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,15 @@
-export let cart = JSON.parse(localStorage.getItem("cart")) || [];
+export let cart = JSON.parse(localStorage.getItem("cart")) || [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+    deliveryOptionId: "1",
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 2,
+    deliveryOptionId: "2",
+  },
+];
 let timeOutIds = {}; //object to store timeOutIds for each product
 
 export function saveToStorage() {
@@ -21,6 +32,7 @@ export function addToCart(productId, newQuantity) {
     cart.push({
       productId,
       quantity: newQuantity,
+      deliveryOptionId : '1'
     });
   }
 
