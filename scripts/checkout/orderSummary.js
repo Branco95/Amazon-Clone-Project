bc2ff4fe9915f3815376dc0295e12e8f7f7cf93a
Branco95 <branquinho95@outlook.com>
2024-10-13 11:52:46 +0100

    Create automated test for orderSummary() using Jasmine

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -12,8 +12,6 @@ import { getDate } from "../utils/date.js";
 export function renderOrderSummary() {
   let cartSummaryHTML = "";
   updateCheckoutQuantity();
-  console.log("enter");
-
   cart.forEach((cartItem) => {
     const productId = cartItem.productId;
 
@@ -26,7 +24,7 @@ export function renderOrderSummary() {
     const dateString = getDate(deliveryOption.deliveryDays);
 
     cartSummaryHTML += `
-    <div class="cart-item-container js-cart-item-container-${
+    <div class="cart-item-container js-cart-item-container js-cart-item-container-${
       matchingProduct.id
     }">
             <div class="delivery-date">Delivery date: ${dateString}</div>
@@ -44,14 +42,17 @@ export function renderOrderSummary() {
                 <div class="product-price">€${formatCurrency(
                   matchingProduct.priceCents
                 )}</div>
-                <div class="product-quantity">
+                <div class="product-quantity js-product-quantity-${
+                  matchingProduct.id
+                }">
                   <span> Quantity: <span class="quantity-label">${
                     cartItem.quantity
                   }</span> </span>
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id=${
+                  <span class="delete-quantity-link link-primary js-delete-link
+                  js-delete-link-${matchingProduct.id}" data-product-id=${
                     matchingProduct.id
                   }>
                     Delete
---
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -0,0 +1,69 @@
+import { loadFromStorage } from "../../data/cart.js";
+import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
+import { cart } from "../../data/cart.js";
+
+describe("test suite: renderOrderSummary()", () => {
+  const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
+  const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
+  //   beforeEach will run before each test
+  beforeEach(() => {
+    spyOn(localStorage, "setItem");
+
+    document.querySelector(".js-test-container").innerHTML = `
+          <div class='js-order-summary'></div>
+          <div class='js-delete-link'></div>
+          <div class='js-delivery-option'></div>
+          <div class='js-checkout-quantity'></div>
+          <div class='js-delete-link'></div>
+          <div class='js-payment-summary'></div>
+        `;
+
+    spyOn(localStorage, "getItem").and.callFake(() => {
+      return JSON.stringify([
+        {
+          productId: productId1,
+          quantity: 2,
+          deliveryOptionId: "1",
+        },
+        {
+          productId: productId2,
+          quantity: 1,
+          deliveryOptionId: "2",
+        },
+      ]); // Mocking the localStorage getItem with an existing product
+    });
+
+    loadFromStorage();
+
+    renderOrderSummary();
+  });
+
+  //   afterEach will run after each test
+  afterEach(() => {
+    document.querySelector(".js-test-container").innerHTML = ``;
+  });
+
+  it("displays the cart", () => {
+    expect(document.querySelectorAll(".js-cart-item-container").length).toEqual(
+      2
+    );
+    expect(
+      document.querySelector(`.js-product-quantity-${productId1}`).innerText
+    ).toContain("Quantity: 2");
+    expect(
+      document.querySelector(`.js-product-quantity-${productId2}`).innerText
+    ).toContain("Quantity: 1");
+  });
+
+  it("removes a product", () => {
+    document.querySelector(`.js-delete-link-${productId1}`).click(); //after clicking delete we should have just 1 item
+    expect(document.querySelectorAll(".js-cart-item-container").length).toEqual(
+      1
+    );
+    expect(
+      document.querySelector(`js-cart-item-container-${productId1}`)
+    ).toEqual(null);
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual(productId2);
+  });
+});
